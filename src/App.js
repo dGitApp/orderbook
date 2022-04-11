8ebcec3f33d7d811dfedb1f0d400e96b20dc0f68
stemerlini <s.merlini19@imperial.ac.uk>
2022-04-11 21:37:40 +0100

    styled front-end page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/icons/dGitIconGreen.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className = 'App'>
+      <header className='App-header'> 
+        <img className='App-logo' src = {logo} alt = 'logo' />
+        <text> dGit - Orderbook v0.1.0 </text>
       </header>
+      <div className='order-container'> 
+        list of orders
+      </div>
     </div>
   );
 }
