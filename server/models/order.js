1759e7256de48441f28865fd53fa83fc8065c846
stemerlini <s.merlini19@imperial.ac.uk>
2022-04-12 23:47:49 +0100

    complete order schema

--- server/models/order.js
+++ server/models/order.js
@@ -3,15 +3,16 @@
 const mongoose = require('mongoose');
 
 const orderSchema = mongoose.Schema({
-    erc20Token: String,
-    erc20TokenAmount: String,
-    nftToken: String,
-    nftTokenId: String,
-    nftTokenAmount: String,
-    nftType: String,
-    sellOrBuyNft: 'buy' | 'sell',
-    chainId: String,
-    order: SignedNftOrderV4Serialized,
-    metadata: Record<string, string> | null,
-    }
+        "erc20Token": String,
+        "erc20TokenAmount": String,
+        "nftToken": String,
+        "nftTokenId": String,
+        "nftTokenAmount": String,
+        "nftType": String,
+        "sellOrBuyNft": String,
+        "chainId": String,
+        "order": {},
+        "orderStatus": {},
+        "metadata": {}
+      }      
 )
