94b82fd0f62819cc012dfc3f03a668dc0b7b6e91
stemerlini <s.merlini19@imperial.ac.uk>
2022-04-15 19:07:20 +0100

    tested post command with json format --> require displaying orders

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
 require('dotenv/config')
 
 const app = express();
+
+app.use(bodyParser.json());
 
 // import routes
 const orderbookRoute = require('./routes/orderbook');
