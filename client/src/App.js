1328284e143d99bddfed75d97ccbda93e23c2157
stemerlini <s.merlini19@imperial.ac.uk>
2022-04-24 10:32:02 +0100

    starting search options and integrated searchOrderbook

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
 import './App.css';
 import logo from './assets/icons/dGitIconGreen.png'
+import { Box, ChakraProvider } from '@chakra-ui/react'
 
 
 
 function App() {
   return (
+    <ChakraProvider>
     <div className = 'App'>
       <header className='App-header'> 
         <img className='App-logo' src = {logo} alt = 'logo' />
         <text> dGit - Orderbook v0.1.0 </text>
       </header>
+      <Box className='App-Search'>
+      </Box>
       <div className='order-container'> 
         list of orders
       </div>
     </div>
+    </ChakraProvider>
   );
 }
 
--- client/src/components/orderList.ts
+++ client/src/components/orderList.ts
@@ -1,5 +1,7 @@
 
-let API_address = 'https://api.trader.xyz/orderbook/orders'
+// Using the nft swap sdk library --> use the function search orders from orderbook.ts and create a react component for fetching orders
+import {useReducer, useEffect, useState, useRef} from "react"
+import { searchOrderbook } from "@traderxyz/nft-swap-sdk/dist/sdk/v4/orderbook";
 
 export interface OrderbookRequestOptions {
   rootUrl?: string;
@@ -18,11 +20,12 @@ export interface SearchOrdersParams {
   status?: 'open' | 'filled' | 'expired' | 'cancelled' | 'all';
 }
 
-export function orderList(params:SearchOrdersParams) {
-
+export function orderList(params:SearchOrdersParams,API_URL:OrderbookRequestOptions) {
+    
+    const fetchResults = await searchOrderbook(params,API_URL)
+    
     return (
-        <div> 
-        
+        <div>
         </div> 
     )
 }
