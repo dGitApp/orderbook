c24aa2b8ceb6c715d9ada5891c65ace5de460b62
stemerlini <s.merlini19@imperial.ac.uk>
2022-04-30 12:03:44 +0100

    required including import for cards component

--- client/src/components/Order.js
+++ client/src/components/Order.js
@@ -2,12 +2,8 @@ import React from 'react'
 import Stack from '@mui/material/Stack';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 
 import { styled } from '@mui/material/styles';
 import './Order.css'
@@ -24,22 +20,29 @@ function Order() {
   return (
     <Box sx = {{width: '100%'}}>
       <Stack spacing={1}>
-        <Item elevation={5} >
-          <Table sx={{ width: '500px'}}>
-            <TableHead>
-              <TableRow>
-                <TableCell> Dessert (100g serving)</TableCell>
-                <TableCell> Dessert (100g serving)</TableCell>
-                <TableCell> Dessert (100g serving)</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableCell align="right">cell1</TableCell>
-              <TableCell align="right">cell2</TableCell>
-              <TableCell align="right">cell3</TableCell>
-            </TableBody>
-          </Table>
-        </Item>
+        <Card sx={{ maxWidth: 345 }}>  
+          <CardContent>
+            <Typography variant="body2" color="text.secondary">
+              order content
+            </Typography>
+          </CardContent>
+          <CardActions disableSpacing>
+            <ExpandMore
+              expand={expanded}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+            </ExpandMore>
+          </CardActions>
+          <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <CardContent>
+              <Typography paragraph> full description </Typography>
+            </CardContent>
+          </Collapse>
+        </Card>
+        <Item elevation={5} > </Item>
         <Item elevation= {5}> order 2 </Item>
       </Stack>
     </Box>
